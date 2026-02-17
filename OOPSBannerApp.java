38556fec11040b366420722fbc5e4a6ec18ced4c
tharun-1509 <tp0111@srmist.edu.in>
2026-02-17 14:25:16 +0530

    UC2 Added OOPS ASCII banner with string concatenation

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+public class OOPSBannerApp {
+
+    // Main method to run the banner display
+    public static void main(String[] args) {
+        
+        /* * Each letter (O, O, P, S) is built using manual string concatenation (+).
+         * Each letter block is 9 characters wide to ensure vertical alignment.
+         */
+        
+        System.out.println("   *    " + " " + "   *    " + " " + " *  *    " + " " + "   *****  ");
+        System.out.println(" *   *  " + " " + " *   *  " + " " + " *    *  " + " " + "  **      ");
+        System.out.println("*     * " + " " + "*     * " + " " + " *    * " + " " + " **       ");
+        System.out.println("*     * " + " " + "*     * " + " " + " ** *  " + " " + "  *****   ");
+        System.out.println("*     * " + " " + "*     * " + " " + " *       " + " " + "      ** ");
+        System.out.println(" *   *  " + " " + " *   *  " + " " + " *       " + " " + "     **  ");
+        System.out.println("   *    " + " " + "   *    " + " " + " *       " + " " + " *****   ");
+    }
+}
